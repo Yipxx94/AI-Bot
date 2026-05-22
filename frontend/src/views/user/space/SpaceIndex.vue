db0541a0cbbed8607e0a9679d35129170d95f7dd
yexx <yipxx@foxmail.com>
2026-05-23 01:44:40 +0800

    【feature】实现前端界面路由

---
+++ frontend/src/views/user/space/SpaceIndex.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {useRoute} from "vue-router";
+
+// 调用当前页面参数
+const route = useRoute();
+
+</script>
+
+<template>
+个人空间: {{ route.params.user_id }}
+</template>
+
+<style scoped>
+
+</style>

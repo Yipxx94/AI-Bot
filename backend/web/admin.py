1925f2c538bb4d3a080aba85eb7c26de664ab3a2
yexx <yipxx@foxmail.com>
2026-05-23 02:53:11 +0800

    【feature】实现后端数据库

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from web.models.user import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
+
+# 把自定义的数据库类在admin界面显示
